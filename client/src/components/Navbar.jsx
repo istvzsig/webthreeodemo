53049c12acf0a0d462d45b86041b478c31c5110d
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-04 12:05:12 +0700

    update navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 export default function Navbar() {
+    const menu = ['Services', 'Transactions', 'Contact', 'Login'];
     return (
-        <nav className="flex justify-center h-20">
-            
+        <nav className="flex flex-col md:flex-row justify-between px-[5vw] py-[2vh] ">
+            <h1 className="cursor-pointer font-bold text-[5vw]">LOGO</h1>
+            <ul className="flex flex-col md:flex-row items-center -mr-[1vw]">
+            {
+                menu.map((item, i) => <div className={"cursor-pointer rounded p-[1vw] mx-[2vw] "
+                    + (item == 'Login' ? 'px-[2vw] bg-[#2546bd] hover:bg-[#ffa555]' : '')}>{item}</div>)
+            }
+            </ul>
         </nav>
     )
 }
-

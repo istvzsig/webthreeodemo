aa96a74eabdcd6de752d0c21a223dbf14caa6005
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-06 16:56:48 +0700

    add components to hero section

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,6 @@
+export default function Button() {
+    return (
+        <button
+            className="text-2xl uppercase my-[.5rem] py-[.7rem] w-full bg-[#2546bd] hover:bg-[#ffa555] rounded">Send</button>
+    )
+}
---
+++ client/src/components/Input.jsx
@@ -0,0 +1,8 @@
+export default function Input(props) {
+    return (
+        <input
+            placeholder={props.placeholder}
+            className='text-black font-light rounded my-[.5rem] px-[10px] py-[.7rem]'
+        />
+    )
+}

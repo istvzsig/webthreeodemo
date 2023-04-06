79b2a310ce7c2c42a0cdcfbc105978749c045bcc
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-06 22:08:22 +0700

    update hero and form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,33 +1,18 @@
 import Loader from './Loader';
-
-function Input(classProps=null) {
-    return (
-        <input className='text-black font-light rounded my-[.5rem] px-[10px] py-[.7rem]' />
-    )
-}
-
-function Button() {
-    return (
-        <button
-            className="text-2xl uppercase my-[.5rem] py-[.7rem] w-full bg-[#2546bd] hover:bg-[#ffa555] rounded">Send</button>
-    )
-}
-
-function sendTransaction(e) {
-    e.preventDefault();
-    console.log(e);
-}
+import Input from './Input';
+import Button from './Button';
+import DemoCard from './DemoCard';
 
 export default function Form() {
-    const items = ["Enter xy", "ASDSAD", "Marosan attila Gyorgy"];
-    let isLoading = true;
+    const items = ["Enter something 1", "Enter something 2", "Enter something 3"];
+    let isLoading = false;
     return (
-        <form className="flex flex-col self-center w-[100%] h-[100%] p-[2vw] pt-0 max-w-[420px]">
-            {
-                items.map((input, i) => 
-                    <Input  key={i} />)
-            }
-            {isLoading ? <Loader /> : <Button />}
-        </form>
+        <div className='self-center md:max-w-600 max-w-full'>
+            <DemoCard />
+            <form className="flex flex-col self-center sm:w-72 w-full">
+                {items.map((input, i) => <Input key={i} placeholder={input} />)}
+                {isLoading ? <Loader /> : <Button />}
+            </form>
+        </div>
     )
 }

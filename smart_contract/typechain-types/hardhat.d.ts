757488516a4ee8f492b356d654aab624a15206e5
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-09 06:07:29 +0700

    solidity transactions and fix

--- smart_contract/typechain-types/hardhat.d.ts
+++ smart_contract/typechain-types/hardhat.d.ts
@@ -16,12 +16,21 @@ declare module "hardhat/types/runtime" {
       name: "Lock",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Lock__factory>;
+    getContractFactory(
+      name: "Transactions",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.Transactions__factory>;
 
     getContractAt(
       name: "Lock",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.Lock>;
+    getContractAt(
+      name: "Transactions",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Transactions>;
 
     // default types
     getContractFactory(
--- smart_contract/typechain-types/index.ts
+++ smart_contract/typechain-types/index.ts
@@ -2,5 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { Lock } from "./Lock";
+export type { Transactions } from "./Transactions";
 export * as factories from "./factories";
 export { Lock__factory } from "./factories/Lock__factory";
+export { Transactions__factory } from "./factories/Transactions__factory";

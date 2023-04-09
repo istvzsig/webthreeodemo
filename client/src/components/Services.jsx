80d92bd237b735ebdbd2fae7c0ba1f0575bb17eb
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-09 10:36:12 +0700

    update services component

---
+++ client/src/components/Services.jsx
@@ -0,0 +1,7 @@
+export default function Services() {
+    return (
+        <section className="flex md:flex-row flex-col md:justify-evenly py-[5vh]">
+            <h1 className="text-5xl text-center md:text-left font-semibold pb-[2vh]">Services</h1>
+        </section>
+    )
+}

331881cddee1fbed50dba04a1e294125f97f926b
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-10 20:21:00 +0700

    add icons components

---
+++ client/src/components/icons.jsx
@@ -0,0 +1,9 @@
+import {BsShieldFillCheck} from 'react-icons/bs';
+import {BiSearchAlt} from 'react-icons/bi';
+import {RiHeart2Fill} from 'react-icons/ri';
+
+export const serviceIcons = {
+    sec: <BsShieldFillCheck fontSize={55} color={"red"} />,
+    exc: <BiSearchAlt fontSize={55} color={"white"}/>,
+    trans: <RiHeart2Fill fontSize={55} color={"red"}/>
+}

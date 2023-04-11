7785335acdaa23ffbd209e3a96c4850f289690c5
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-11 15:27:19 +0700

    add utils and update hardhat config

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -6,8 +6,8 @@ const config: HardhatUserConfig = {
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
-      // replace this with your own test address
-      accounts: ['a425b0749cfd5b240a4d8fc7dfb4fd3e7e3e00ad1b0d5ba709f459cd3205bde8'],
+      // replace this with your own test private key
+      accounts: [],
     }
   }
 }
---
+++ smart_contract/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractAbi = abi.abi;
+export const contractAddress = '0xcf0958a9ad1e7362350c1bd4615cb8380d9e6852f4816a09bf7506d4ad8f1c55';

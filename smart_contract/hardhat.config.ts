b3bb0e58b99584effaf47f5ecaf409a8923a67e6
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-04-11 13:56:07 +0700

    user config for hardhat

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,11 +1,14 @@
-require('@nomiclabs/hardhat-waffle');
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: '0.8.0',
   networks: {
     localhost: {
       url: 'http://localhost:8545',
-      accounts: ['0x36411CE1554895d7d858a2594Cb6E28E9C575070'],
+      accounts: ['0xf395ec4903f9592c33bc9c5002761e5dd5c78b7a41297f594f6c2e9fce8509e0'],
     }
   }
 }
+
+export default config;
